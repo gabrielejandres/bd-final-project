4b7e3afd51ebf0abba6b60a99dacbdc84741e4e7
Gabriele Jandres <gabriele.jandres@bemobi.com.br>
2022-03-05 11:28:42 -0300

    add new button layout

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -14,9 +14,9 @@ export default function Home(){
             <Logo/>
         </div>
         <div className="buttons-area">
-          <Button label={'Jogar'} page={'play'} width={'9.12vw'}/>
-          <Button label={'Ajuda'} page={'help'} width={'9.12vw'}/>
-          <Button label={'Ranking'} page={'ranking'} width={'9.12vw'}/>
+          <Button label={'JOGAR'} page={'play'} width={'12.12vw'}/>
+          <Button label={'AJUDA'} page={'help'} width={'12.12vw'}/>
+          <Button label={'RANKING'} page={'ranking'} width={'12.12vw'}/>
         </div>
       </div>
       <HomeFooter/>
