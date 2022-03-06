ed49040c9095b1ed993f4bf50f1fc620ce6f86cb
Gabriele Jandres <gabriele.jandres@bemobi.com.br>
2022-03-06 14:00:11 -0300

    + ranking

--- frontend/src/pages/Ranking/index.jsx
+++ frontend/src/pages/Ranking/index.jsx
@@ -1,16 +1,42 @@
 import './style.css'
+import { useState } from 'react';
 import Footer from '../../components/Footer/Home/index.jsx';
+import { MdSportsScore, MdOutlineArrowBackIosNew } from 'react-icons/md';
+import { FaMedal } from 'react-icons/fa'; 
 
-export default function Ranking(){
+export default function Ranking() {
+
+  const [users, setUsers] = useState([{'name': 'User 1', 'score': '300'}, {'name': 'User 2', 'score': '200'},
+  {'name': 'User 3', 'score': '200'}, {'name': 'User 4', 'score': '200'}, {'name': 'User 5', 'score': '200'} ]);
+
   return(
     <div className="main-page">
       <div className="container">
-        <div className="ranking">
-            <ul>
-                <li><span>1.</span> Fulano </li>
-                <li><span>2.</span> Ciclano </li>
-            </ul>
+        <div className="header">
+          <MdOutlineArrowBackIosNew className="goBack" />
+          <h1> Ranking </h1>
         </div>
+          <ul>
+            {users.map((user, index) => {
+              return (
+                <li key={index}>
+                  <div className="user">
+                    <div className="position">
+                      { index + 1 }
+                    </div>
+                    { user.name } 
+                    { index === 0 && <FaMedal className="icon gold" /> }
+                    { index === 1 && <FaMedal className="icon silver" /> }
+                    { index === 2 && <FaMedal className="icon copper" /> }
+                  </div>
+                  <span className="score"> 
+                    <MdSportsScore className="icon"/>
+                    { user.score } 
+                  </span>
+                </li>
+              )
+            })}
+          </ul>
         <Footer/>
       </div>
     </div>
