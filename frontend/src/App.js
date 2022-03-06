9278a72fa38f7ac2e38c830a7630ea938f35f4ad
Gabriele Jandres <gabriele.jandres@bemobi.com.br>
2022-03-05 23:43:26 -0300

    refactoring css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Login from './pages/Login/index.jsx'
 import Home from './pages/Home/index.jsx'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
-
 function App() {
   return (
     <div>
--- frontend/src/components/Logo/index.jsx
+++ frontend/src/components/Logo/index.jsx
@@ -5,4 +5,4 @@ export default function Logo(){
     return(
         <img src={LogoImg} />
     )
-  }
+}
