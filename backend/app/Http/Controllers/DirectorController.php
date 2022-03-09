5a182a9ec9c90d61c1d20caed0b88630e7b6ea3b
Gabriele Jandres Cavalcanti <gabrielejandres@gmail.com>
2022-03-08 22:13:31 -0300

    + Q6

--- backend/app/Http/Controllers/DirectorController.php
+++ backend/app/Http/Controllers/DirectorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Director;
 use App\Models\Actor;
+use App\Models\Genre;
 
 class DirectorController extends Controller
 {
@@ -105,6 +106,51 @@ class DirectorController extends Controller
         // getting options
         $options = Director::select('name')
                             ->whereNotIn('directors.name', Actor::select('name'))
+                            ->inRandomOrder()
+                            ->limit(3)
+                            ->get();        
+        
+        $optionsArray = [];
+        for ($i = 0; $i < count($options); $i++) {
+            $optionsArray[$i] = $options[$i]->name;
+        }
+        array_push($optionsArray, $answer);
+
+        // randomizing options
+        shuffle($optionsArray);
+
+        return response()->json([
+            'question' => $question,
+            'options' => $optionsArray,
+            'answer' => $answer
+        ]);
+    }
+
+    public function getDirectorWithMoreMediasByGenreQuestion() {
+        // making the question
+        $genre = Genre::select('id', 'name')
+                        ->inRandomOrder()
+                        ->limit(1)
+                        ->get()
+                        ->toArray();
+        $question = 'Qual diretor dirigiu mais mídias do gênero ' . $genre[0]['name'] . '?';
+
+        // getting a valid answer
+        $answer = Director::selectRaw('directors.name, count(genre_media.genre_id) as occurrences')
+                        ->join('director_media', 'director_media.director_id', '=', 'directors.id')
+                        ->join('media', 'director_media.media_id', '=', 'media.id')
+                        ->join('genre_media', 'genre_media.media_id', '=', 'media.id')
+                        ->where('genre_media.media_id', $genre[0]['id'])
+                        ->groupBy('genre_media.genre_id', 'directors.name')
+                        ->orderByDesc('occurrences')
+                        ->inRandomOrder()
+                        ->first();
+
+        $answer = $answer ? $answer->name : 'Nenhuma das opções';
+
+        // getting options
+        $options = Director::select('name')
+                            ->where('name', '!=', $answer)
                             ->inRandomOrder()
                             ->limit(3)
                             ->get();        
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -90,6 +90,7 @@ Route::get('/question/notAMovie', [MediaController::class, 'getNotAMovieQuestion
 Route::get('/question/movieByPlatform', [MediaController::class, 'getMovieByPlatformQuestion']);
 Route::get('/question/oldestMovie', [MediaController::class, 'getOldestMovieQuestion']);
 Route::get('/question/oldestSeries', [MediaController::class, 'getOldestSeriesQuestion']);
+Route::get('/question/directorWithMoreMediasByGenre', [DirectorController::class, 'getDirectorWithMoreMediasByGenreQuestion']);
 
 Route::get('/question/seriesWithMoreSeasons', [SeriesController::class, 'getSeriesWithMoreSeasonsQuestion']);
 Route::get('/question/numberOfSeasons', [SeriesController::class, 'getNumberOfSeasonsQuestion']);
@@ -100,8 +101,6 @@ Route::get('/question/directorWithMoreMovies', [DirectorController::class, 'getD
 Route::get('/question/directorAndActor', [DirectorController::class, 'getDirectorAndActorQuestion']);
 
 // To Do
-Route::get('/question/movieByGenreAndActor', [MediaController::class, 'getMovieByGenreAndActor']);
-
-Route::get('/question/directorWithMoreMediasByGenre', [DirectorController::class, 'getDirectorWithMoreMediasByGenre']);
+Route::get('/question/movieByGenreAndActor', [MediaController::class, 'getMovieByGenreAndActorQuestion']);
 
 
